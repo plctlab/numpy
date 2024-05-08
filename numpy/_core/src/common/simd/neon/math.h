94844a1feb7f90b4778ae8bf56797962bd98d704
Yang Liu <liuyang22@iscas.ac.cn>
2024-05-08 12:32:06 +0800

    Try another fix

--- numpy/_core/src/common/simd/neon/math.h
+++ numpy/_core/src/common/simd/neon/math.h
@@ -271,17 +271,16 @@ NPY_IMPL_NEON_REDUCE_MINMAX(max, npy_int64,  s64, >)
 NPY_IMPL_NEON_REDUCE_MINMAX(min, npy_uint64, u64, <)
 NPY_IMPL_NEON_REDUCE_MINMAX(min, npy_int64,  s64, <)
 #elif defined(NPY_HAVE_RVV)
-#define NPY_IMPL_NEON_REDUCE_MINMAX(INTRIN, STYPE, SFX, SFX1, OP)         \
-    NPY_FINLINE STYPE npyv_reduce_##INTRIN##_##SFX(npyv_##SFX a)          \
-    {                                                                     \
-        STYPE al = __riscv_vmv_x_s_##SFX1##m1_##SFX1(vget_low_##SFX(a));  \
-        STYPE ah = __riscv_vmv_x_s_##SFX1##m1_##SFX1(vget_high_##SFX(a)); \
-        return al OP ah ? al : ah;                                        \
+#define NPY_IMPL_NEON_REDUCE_MINMAX(INTRIN, STYPE, SFX, SFX1)                   \
+    NPY_FINLINE STYPE npyv_reduce_##INTRIN##_##SFX(npyv_##SFX a)                \
+    {                                                                           \
+        return __riscv_vmv_x_s_##SFX1##m1_##SFX1(                               \
+            __riscv_vredmaxu_vs_##SFX1##m1_##SFX1##m1(a, vdupq_n_##SFX(0), 2)); \
     }
-NPY_IMPL_NEON_REDUCE_MINMAX(max, npy_uint64, u64, u64, >)
-NPY_IMPL_NEON_REDUCE_MINMAX(max, npy_int64,  s64, i64, >)
-NPY_IMPL_NEON_REDUCE_MINMAX(min, npy_uint64, u64, u64, <)
-NPY_IMPL_NEON_REDUCE_MINMAX(min, npy_int64,  s64, i64, <)
+NPY_IMPL_NEON_REDUCE_MINMAX(max, npy_uint64, u64, u64)
+NPY_IMPL_NEON_REDUCE_MINMAX(max, npy_int64,  s64, i64)
+NPY_IMPL_NEON_REDUCE_MINMAX(min, npy_uint64, u64, u64)
+NPY_IMPL_NEON_REDUCE_MINMAX(min, npy_int64,  s64, i64)
 #else
     #error Something is wrong
 #endif
